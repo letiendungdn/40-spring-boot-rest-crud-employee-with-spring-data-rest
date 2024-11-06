a6cac3605fb571fd2b4d6fc3d121ae6480e75964
letiendungdn <letiendungdt@gmail.com>
2024-11-06 13:49:28 +0700

    first commit

---
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,59 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    // quick and dirty: inject employee dao
+
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
+    }
+
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+        return  theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+        employeeService.deleteById(employeeId);
+        return "Employee with id " + employeeId + " deleted successfully";
+    }
+}
